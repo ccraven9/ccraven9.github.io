786a602fe9948b27f1745d73ee810cb82b2efb57
ccraven9 <c.craven1337@gmail.com>
2023-03-05 23:58:24 -0500

    Adding html and css

---
+++ main.js
@@ -0,0 +1,8 @@
+var nCirlce = document.querySelector('.neumo-circle');
+var resumeURL = "resume/resume.html"
+
+nCirlce.addEventListener('click',
+    function () {
+        window.location.assign(resumeURL);
+    }
+);
