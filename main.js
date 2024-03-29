2b070bbd00227090725261cda1e6a28ecb3ce5d7
ccraven9 <c.craven1337@gmail.com>
2024-03-29 14:57:46 -0400

    Updating with new links, new resume css, updated resume design.

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 const nCirlce = document.querySelector('.neumo-circle');
-const resumeURL = "resume/resume.html"
+const githubBtn = document.getElementById('github');
+const resumeBtn = document.getElementById('resume');
+const linkedBtn = document.getElementById('linkedIn');
+const resumeURL = "resume/resume.html";
+const githubURL = "https://github.com/ccraven9";
+const linkedInURL = "https://www.linkedin.com/in/carter-craven-75a029107";
+
+function navigate(url) {
+    window.location.assign(url);
+}
 
 nCirlce.addEventListener('click',
     function () {
-        window.location.assign(resumeURL);
-    }
-);
+        navigate(resumeURL);
+    });
+
+githubBtn.addEventListener('click',
+    function () {
+        navigate(githubURL);
+    });
+
+resumeBtn.addEventListener('click',
+    function () {
+        navigate(resumeURL);
+    });
+
+linkedBtn.addEventListener('click',
+    function () {
+        navigate(linkedInURL);
+    });
